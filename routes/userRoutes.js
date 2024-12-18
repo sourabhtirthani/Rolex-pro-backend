5c9287eb5b1a6275b5f9e6e76aa9cf5e45794aed
dotcreativetech <dotcreativetech@gmail.com>
2024-12-18 21:15:10 +0530

    change methods to post

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,17 +7,17 @@ import  {    buyProIncome, createProfile, freeRegistration, getProfile, getSelfI
 const router = express.Router();
 
 router.post('/create' , [errorHandler] , createProfile);
-router.patch('/updateData',errorHandler,updateProfile)
-router.patch('/update' ,[upload.fields([{ name: 'profilePicture', maxCount: 1 }]),errorHandler], errorHandler, updateUserProfile )
+router.post('/updateData',errorHandler,updateProfile)
+router.post('/update' ,[upload.fields([{ name: 'profilePicture', maxCount: 1 }]),errorHandler], errorHandler, updateUserProfile )
 router.get('/checkUser',errorHandler,getProfile)
 router.post('/buyProIncome',errorHandler,buyProIncome);
-router.patch('/updateProIncome',errorHandler,updateProIncome);
+router.post('/updateProIncome',errorHandler,updateProIncome);
 router.post('/buyGlobalIncome',errorHandler,buyGlobalIncome);
-router.patch('/updateGlobalIncome',errorHandler,updateGloablIncome);
+router.post('/updateGlobalIncome',errorHandler,updateGloablIncome);
 router.post('/freeResistration',errorHandler,freeRegistration)
 router.get('/selfIncome',errorHandler,getSelfIncome)
 router.post('/selfIncome',errorHandler,postselfIncome);
-router.patch('/selfIncome',errorHandler,updateselfIncome);
+router.post('/selfIncome',errorHandler,updateselfIncome);
 router.get('/preview',errorHandler,previewProfile)
 router.get('/getReferDetails',errorHandler,getReferDetails);
 // package Router 
